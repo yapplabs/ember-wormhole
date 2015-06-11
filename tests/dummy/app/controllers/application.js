d3f2e314851a1555deb34f0da4e96f2234824dd8
Mike North <northm@yahoo-inc.com>
2015-06-11 12:24:20 -0700

    Allow use of arbitrary selectors, instead of only ids

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,12 +4,16 @@ var set = Ember.set;
 
 export default Ember.Controller.extend({
   isShowingModal: false,
+  isShowingModal2: false,
   isShowingSidebarContent: false,
   sidebarId: 'sidebar',
   isInPlace: false,
   actions: {
     toggleModal() {
       this.toggleProperty('isShowingModal');
+    },
+    toggleModal2() {
+      this.toggleProperty('isShowingModal2');
     },
     toggleSidebarContent() {
       this.toggleProperty('isShowingSidebarContent');
--- tests/dummy/app/initializers/add-modals-element.js
+++ tests/dummy/app/initializers/add-modals-element.js
@@ -1,10 +1,21 @@
 /*globals document*/
 function initialize(container, application){
   var rootEl = document.querySelector(application.rootElement);
+
+  var existingModalsEl = document.getElementById('modals');
+
+  if (existingModalsEl) {
+    existingModalsEl.remove();
+  }
+
   var modalContainerEl = document.createElement('div');
   var modalContainerElId = 'modals';
   modalContainerEl.id = modalContainerElId;
   rootEl.appendChild(modalContainerEl);
+
+  var modal2ContainerEl = document.createElement('div');
+  modal2ContainerEl.className = 'modal2';
+  modalContainerEl.appendChild(modal2ContainerEl);
 }
 
 export default {
