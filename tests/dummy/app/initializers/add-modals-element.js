5b01d7ffc4cdad1c75a1fb3f808d37237debe6aa
Cory Forsyth <cory.forsyth@gmail.com>
2016-06-08 14:54:00 -0400

    Fix initializer arity deprecation

--- tests/dummy/app/initializers/add-modals-element.js
+++ tests/dummy/app/initializers/add-modals-element.js
@@ -1,5 +1,8 @@
 /*globals document*/
-function initialize(container, application){
+function initialize(){
+  // In Ember 2.x, initialize is called only with `application` instead of `(container, application)`
+  // See http://emberjs.com/deprecations/v2.x/#toc_initializer-arity
+  var application = arguments[1] || arguments[0];
   var rootEl = document.querySelector(application.rootElement);
   var modalContainerEl = document.createElement('div');
   var modalContainerElId = 'modals';
